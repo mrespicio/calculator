ad898990a17797527c741d3fead763277a9f109e
mrespicio <meganrespicio@hotmail.com>
2023-02-24 12:33:15 -0800

    add functionality to digits, operands, and equals buttons

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const calculatorContainer = document.getElementById('calculator-container');
 const buttons = calculatorContainer.querySelectorAll('button'); // nodelist of all bbuttons
 
-let buttonA = 0;
-let buttonB = 0;
+let numA = 0;
+let numB = 0;
+let op = '';
+let statement = [];
+let currentResults;
 
 buttons.forEach(button => {
-	button.addEventListener('click', () => alert('hello!'))
+	button.addEventListener('click', () => {  // put element in array
+		if(button.id == 'equals'){ //calculate
+			numA = Number(statement[0]);
+			numB = Number(statement[2])
+			currentResults = operate(statement[1], numA, numB)
+			console.log('the current answer is: ' + currentResults)
+			console.log('before modifying: ' + statement)
+			statement.splice(0, 3, currentResults)
+			console.log('after splicing: ' + statement)
+		}
+		statement.push(button.id);
+		if(button.id == 'equals') statement.pop();
+		console.log(statement);
+		//console.log('the current answer is: ' + currentCalc)
+	});
 });
+
 
 function add(a, b){
 	return a + b;
@@ -27,18 +45,18 @@ function divide(a, b){
 function operate(operator, a, b){
 	switch(operator){
 		case 'plus':
-			add(a, b);
+			return add(a, b);
 			break;
 		case 'minus':
-			subtract(a, b);
+			return subtract(a, b);
 			break;
 		case 'times':
-			multiply(a, b);
+			return multiply(a, b);
 			break;
 		case 'divided':
-			divide(a, b);
+			return divide(a, b);
 			break;
 	}
 }
 
-console.log(calculatorContainer);
+console.log(operate('plus', 1, 5))
