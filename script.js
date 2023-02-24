e0deabb7a2cf46df368abfe0a0d2f8ae345fd8db
mrespicio <meganrespicio@hotmail.com>
2023-02-24 14:06:00 -0800

    fix  certain behaviors
    when user clicks on another operand after a 3 button sequence (digit-operand-digit), the
    statement will execute and push the results and new operand into the statement array
    
    when user selects two operands back to back or one operated followed by an equal, the first operand will
    execute on the only digited entered by itself (ex: 2 + * will calculate (2 + 2) * then yield 4 *)
    
    add functionality to clear all and clear entry buttons

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 const calculatorContainer = document.getElementById('calculator-container');
+const digitsContainer = document.getElementById('digits');
 const buttons = calculatorContainer.querySelectorAll('button'); // nodelist of all bbuttons
 
-let numA = 0;
-let numB = 0;
 let op = '';
 let statement = [];
-let currentResults;
+let num = [];
+let numA;
+let numB
+let currentResults;   
 
 buttons.forEach(button => {
-	button.addEventListener('click', () => {  // put element in array
-		if(button.id == 'equals'){ //calculate
-			numA = Number(statement[0]);
-			numB = Number(statement[2])
-			currentResults = operate(statement[1], numA, numB)
-			console.log('the current answer is: ' + currentResults)
-			console.log('before modifying: ' + statement)
-			statement.splice(0, 3, currentResults)
-			console.log('after splicing: ' + statement)
+	button.addEventListener('click', () => {
+
+		// when user clicks number, operand, then equal
+		if(button.id == 'equals' && statement.length == 2){
+			console.log('you clicked a number, operand, then equal');
+			currentResults = operate(statement[1], Number(statement[0]), Number(statement[0]));
+			statement.splice(0, 2, currentResults);
 		}
-		statement.push(button.id);
-		if(button.id == 'equals') statement.pop();
+
+		// when statement is ready to be calculated, with 3 values
+		else if(button.id == 'equals' || 
+			(button.parentElement.id == 'operands' && statement.length == 3)) { 
+				currentResults = operate(statement[1], Number(statement[0]), Number(statement[2])) //calculate results
+				console.log('the current answer is: ' + currentResults); // display current results
+				statement.splice(0, 3, currentResults); // remove previous elements from array, replaces index 0 w/ current results
+				if(button.parentElement.id == 'operands') statement.push(button.id);
+		}
+
+		else if(button.parentElement.id == 'operands' && statement.length == 2){
+			currentResults = operate(statement[1], Number(statement[0]), Number(statement[0]));
+			statement.splice(0, 2, currentResults);
+			statement.push(button.id);
+			console.log('you clicked: ' + button.id)
+			console.log('the results are: ' + currentResults);
+			console.log('clicked two operands back to back')
+		}
+
+		else if(button.id == 'clear-all'){ //clear entire array
+			statement = [];
+		}
+		else if(button.id == 'clear-entry'){ //clear last entry
+			statement.pop();
+			console.log(statement)
+		}
+		else {
+			statement.push(button.id); 
+		}
 		console.log(statement);
-		//console.log('the current answer is: ' + currentCalc)
 	});
 });
-
 
 function add(a, b){
 	return a + b;
@@ -58,5 +83,3 @@ function operate(operator, a, b){
 			break;
 	}
 }
-
-console.log(operate('plus', 1, 5))
