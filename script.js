9e5a6474d142ea7bdbc567e831fdb8d340ba7653
mrespicio <meganrespicio@hotmail.com>
2023-02-24 16:47:29 -0800

    cleanup code and make array clear if new statement

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 const calculatorContainer = document.getElementById('calculator-container');
-const digitsContainer = document.getElementById('digits');
-const buttons = calculatorContainer.querySelectorAll('button'); // nodelist of all bbuttons
+const buttons = calculatorContainer.querySelectorAll('button'); // nodelist of all buttons
 
 let statement = [];
 let num = '';
 let currentResults;
 
-
 buttons.forEach(button => {
 	button.addEventListener('click', () => {
 		// if click digit, push into digit array
 		if(button.parentElement.id == 'digits'){
-			// clear if new number
+			// if number in statement was calculated, clear it
+			if(statement.length == 1) statement = [];
 			num += button.id;
 			console.log('current Number is: ' + num)
 		}
-
-		//else if(statement.length == 1) statement.push(button.id);
 
 		// click on operand or equals
 		else if(button.parentElement.id == 'operands' || button.id == 'equals'){
@@ -28,13 +25,11 @@ buttons.forEach(button => {
 			// only push operand, no need to push equals
 			if(button.id != 'equals') {statement.push(button.id)}; 
 
-
-
-			// number - operand - operand/ ......equals
+			// number - operand - operand/equals
 			// if you click another operand, check if statement already has number and operand
 			// calculation will take place using digit by itself and first operand
 			if(
-				(button.parentElement.id == 'operands' && 
+			(button.parentElement.id == 'operands' && 
 			typeof statement[0] == 'number' && 
 			typeof statement[1] == 'string' &&
 			typeof statement[2] == 'string') ||
@@ -42,9 +37,7 @@ buttons.forEach(button => {
 			typeof statement[0] == 'number' &&
 			typeof statement[1] == 'string' &&
 			typeof statement[2] == 'undefined')
-			) //if
-			{
-				console.log('hello');
+			){
 				currentResults = operate(statement[1], Number(statement[0]), Number(statement[0]))
 				console.log('the current answer is: ' + currentResults); // display current results
 				statement.splice(0, 3, currentResults); // remove previous elements from array, replaces index 0 w/ current results
@@ -61,37 +54,21 @@ buttons.forEach(button => {
 				num = '';
 			}
 		} //elseif operand or equals
-
+		
 		else if(button.id == 'clear-all'){ //clear entire array
 			statement = [];
 		}
 		else if(button.id == 'clear-entry'){ //clear last entry
 			statement.pop();
 		}
-		/*
-		else {
-			statement.push(button.id); 
-		} */
-
 		console.log(statement);
 	}); //eventclicker
 }); //foreach
 
-function add(a, b){
-	return a + b;
-}
-
-function subtract(a, b){
-	return a - b;
-}
-
-function multiply(a, b){
-	return a * b;
-}
-
-function divide(a, b){
-	return a/b;
-}
+const add = (a, b) =>  a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a/b;
 
 function operate(operator, a, b){
 	switch(operator){
